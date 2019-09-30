f7ded57aeca39ef7d50056d6cfa696f567fc3ba0
Ranjeet Kumar <ranjee970@gmail.com>
2019-09-30 23:13:09 +0530

    Add files via upload

---
+++ HotelCatalouge/HotelCatalouge/src/test/java/com/example/HotelCatalouge/HotelCatalougeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.HotelCatalouge;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HotelCatalougeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
